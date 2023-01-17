bcecd047afec8b1556b9a0b0feb1a6d610609606
Julio Izquierdo Mejia <julio.izquierdo.mejia@gmail.com>
2023-01-17 09:23:51 -0500

    for works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
+import { InspectorService } from './services/inspector.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { PagesModule } from './pages/pages.module';
     HttpClientModule,
     PagesModule
   ],
-  providers: [],
+  providers: [{provide:HTTP_INTERCEPTORS, useClass:InspectorService, multi:true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/inspector.service.ts
+++ src/app/services/inspector.service.ts
@@ -1,9 +1,28 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InspectorService {
+
+export class InspectorService implements HttpInterceptor{
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    
+    console.log('intercetado');
+    
+    const newReq = req.clone({setHeaders:{
+      Authorization: 'Bearer 01010101010101'
+    }})
+    
+    return next.handle(newReq);
+    
+  }
 
   constructor() { }
+
 }
